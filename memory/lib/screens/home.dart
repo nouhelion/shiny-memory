1bf4aab9c5cc6e0fe5b303008bb87a4953a1aa07
Nouhelion <etu_kamel.nouhaila@emsi-edu.ma>
2022-11-13 18:34:40 +0100

    avatar

--- memory/lib/screens/home.dart
+++ memory/lib/screens/home.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
 import '../constants/colors.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: RemWhite,
-          title: Row (children : [
-            Icon(
-              Icons.menu,
-              color: RemBlack,
-              size: 30,
+        backgroundColor: RemWhite,
+        title:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.menu,
+            color: RemBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset('assets/images/cat.png'),
             ),
-          ]),
+          ),
+        ]),
       ),
       body: Container(
         child: Text('This is home screen'),
